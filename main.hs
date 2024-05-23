4e117214754e2bbbea25c261c5288b2f05095599
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-05-23 20:44:17 +0300

    using pauses instead of dotted notes

--- main.hs
+++ main.hs
@@ -2,10 +2,12 @@
 
 import Euterpea
 
-n x = x 4 qn
+p :: Octave -> Dur -> Music Pitch
+p o d = rest 1
 
-p x = [x 4 dqn]
+n :: (Octave -> Dur -> Music Pitch) -> Music Pitch
+n x = x 4 wn
 
-abcSong = playDev 2 $ line (map n [c, c, g, g, a, a] ++ p g ++ map n [f, f, e, e, d, d] ++ p c ++ map n [g, g, f, e, e] ++ p d ++ map n [g, g, f, e, e] ++ p d ++ map n [c, c, g, g, a, a] ++ p g ++ map n [f, f, e, e, d, d] ++ p c)
+abcSong = playDev 2 $ tempo 4 (line (map n [c, c, g, g, a, a, g, p, f, f, e, e, d, d, c, p, g, g, f, e, e, d, p, g, g, f, e, e, d, p, c, c, g, g, a, a, g, p, f, f, e, e, d, d, c]))
 
 main = abcSong
