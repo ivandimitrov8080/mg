4abf2b6a3cf726ebd19adfb99e4a6340e8e1235e
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-05-23 15:50:49 +0300

    full abc song

--- main.hs
+++ main.hs
@@ -4,7 +4,8 @@ import Euterpea
 
 n x = x 4 qn
 
-beginner = playDev 2 $ line (map n [c, c, g, g, a, a, g])
+p x = [x 4 dqn]
 
-main = do
-  beginner
+abcSong = playDev 2 $ line (map n [c, c, g, g, a, a] ++ p g ++ map n [f, f, e, e, d, d] ++ p c ++ map n [g, g, f, e, e] ++ p d ++ map n [g, g, f, e, e] ++ p d ++ map n [c, c, g, g, a, a] ++ p g ++ map n [f, f, e, e, d, d] ++ p c)
+
+main = abcSong
