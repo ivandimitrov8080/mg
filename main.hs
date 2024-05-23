b4bafa5efeb3743bb45aa13170ce0e6086aa8abc
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-05-23 21:56:00 +0300

    attempt at lateralus

--- main.hs
+++ main.hs
@@ -5,9 +5,14 @@ import Euterpea
 p :: Octave -> Dur -> Music Pitch
 p o d = rest 1
 
-n :: (Octave -> Dur -> Music Pitch) -> Music Pitch
-n x = x 4 wn
+h :: (Octave -> Dur -> Music Pitch) -> Music Pitch
+h x = x 4 qn
 
-abcSong = playDev 2 $ tempo 4 (line (map n [c, c, g, g, a, a, g, p, f, f, e, e, d, d, c, p, g, g, f, e, e, d, p, g, g, f, e, e, d, p, c, c, g, g, a, a, g, p, f, f, e, e, d, d, c]))
+l :: (Octave -> Dur -> Music Pitch) -> Music Pitch
+l x = x 2 qn
 
-main = abcSong
+abcSong = playDev 2 $ tempo 4 (line (map h [c, c, g, g, a, a, g, p, f, f, e, e, d, d, c, p, g, g, f, e, e, d, p, g, g, f, e, e, d, p, c, c, g, g, a, a, g, p, f, f, e, e, d, d, c]))
+
+lateralus = playDev 2 $ instrument AcousticGuitarSteel (line [d 2 qn, a 4 qn, g 4 sn, a 4 sn, a 4 en, c 4 qn, c 4 en, a 4 en, a 4 qn])
+
+main = lateralus
