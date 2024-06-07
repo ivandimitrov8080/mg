f4fd7c7f9a1bb99dc08f97e862694bf17d894951
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-06-07 12:15:33 +0200

    trying to do it

--- main.hs
+++ main.hs
@@ -13,6 +13,6 @@ l x = x 2 qn
 
 abcSong = playDev 2 $ tempo 4 (line (map h [c, c, g, g, a, a, g, p, f, f, e, e, d, d, c, p, g, g, f, e, e, d, p, g, g, f, e, e, d, p, c, c, g, g, a, a, g, p, f, f, e, e, d, d, c]))
 
-lateralus = playDev 2 $ instrument AcousticGuitarSteel (line [d 2 qn, a 4 qn, g 4 sn, a 4 sn, a 4 en, c 4 qn, c 4 en, a 4 en, a 4 qn])
+lateralus = playDev 2 $ instrument AcousticGuitarSteel (tempo 1 (line [d 2 den, a 4 dqn, g 4 en, a 4 sn, a 4 qn, c 4 qn, c 4 qn, a 4 qn, a 4 qn]))
 
 main = lateralus
