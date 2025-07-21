385f33af54fd407a9b00c489b4481f9ff0fee948
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-07-21 23:04:15 +0200

    add nvim

--- flake.nix
+++ flake.nix
@@ -1,13 +1,35 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.config.url = "github:ivandimitrov8080/configuration.nix";
+  # nvim config helper
+  inputs.nixvim.url = "github:nix-community/nixvim";
+  inputs.nixvim.inputs.nixpkgs.follows = "nixpkgs";
+  # neovim latest version
+  inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  inputs.neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      config,
+      nixvim,
+      neovim-nightly-overlay,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      nvim = (nixvim.legacyPackages.${system}.makeNixvim config.nixvimConfigs.default).extend {
+        package = neovim-nightly-overlay.packages.${system}.default;
+        plugins = {
+          lsp.servers = {
+            hls.enable = true;
+            hls.installGhc = false;
+          };
+          haskell-scope-highlighting.enable = true;
+        };
+      };
     in
     {
-
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           (ghc.withPackages (
@@ -17,6 +39,7 @@
             ]
           ))
           timidity
+          nvim
         ];
       };
     };
